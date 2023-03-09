fb8794bda4e92cdb964e14f30ea04630b42c581d
Marek Zelinka <mzelinka17@gmail.com>
2023-03-09 09:47:19 +0100

    solve 9.18 - catch errors

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
+import axios from "axios";
 import { FormEvent, useEffect, useState } from "react";
 import { createDiary, getAllDiaries } from "./services/diary";
 import { NonSensitiveDiaryEntry } from "./types";
 
 export function App() {
   const [diaries, setDiaries] = useState<NonSensitiveDiaryEntry[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   useEffect(() => {
     getAllDiaries().then(setDiaries);
@@ -12,33 +14,44 @@ export function App() {
   async function addDiary(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
+    if (errorMessage) {
+      setErrorMessage(null);
+    }
+
     const form = event.currentTarget;
     const formData = new FormData(form);
 
-    const entry = await createDiary({
-      date: String(formData.get("date")),
-      weather: String(formData.get("weather")),
-      visibility: String(formData.get("visibility")),
-      comment: String(formData.get("comment")),
-    });
+    try {
+      const entry = await createDiary({
+        date: String(formData.get("date")),
+        weather: String(formData.get("weather")),
+        visibility: String(formData.get("visibility")),
+        comment: String(formData.get("comment")),
+      });
 
-    setDiaries((diaries) => diaries.concat(entry));
+      setDiaries((diaries) => diaries.concat(entry));
 
-    form.reset();
-    (form.elements.item(0) as HTMLElement)?.focus();
+      form.reset();
+      (form.elements.item(0) as HTMLElement)?.focus();
+    } catch (error) {
+      if (axios.isAxiosError<string>(error)) {
+        setErrorMessage(error.response?.data || error.message);
+      }
+    }
   }
 
   return (
     <div>
       <h2>Add new entry</h2>
+      {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
       <form onSubmit={addDiary}>
         <div>
           <label htmlFor="date">Date</label>{" "}
-          <input type="date" name="date" id="date" required />
+          <input type="date" name="date" id="date" />
         </div>
         <div>
           <label htmlFor="weather">Weather</label>{" "}
-          <select name="weather" id="weather" required>
+          <select name="weather" id="weather">
             <option></option>
             <option value="sunny">Sunny</option>
             <option value="rainy">Rainy</option>
@@ -49,7 +62,7 @@ export function App() {
         </div>
         <div>
           <label htmlFor="visibility">Visibility</label>{" "}
-          <select name="visibility" id="visibility" required>
+          <select name="visibility" id="visibility">
             <option></option>
             <option value="great">Great</option>
             <option value="good">Good</option>
@@ -59,7 +72,7 @@ export function App() {
         </div>
         <div>
           <label htmlFor="comment">Comment</label>{" "}
-          <input type="text" name="comment" id="comment" required />
+          <input type="text" name="comment" id="comment" />
         </div>
         <button type="submit">Add</button>
       </form>
